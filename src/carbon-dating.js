6fa1691cd72ada39b115d9433d30a885e26ed207
Hanna Khanko <ahanko802@gmail.com>
2022-04-27 22:51:09 +0300

    init: add solutions for 3 tasks

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -15,11 +15,16 @@ const HALF_LIFE_PERIOD = 5730;
  * 
  * dateSample('1') => 22387
  * dateSample('WOOT!') => false
- *
+ * 
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if(typeof sampleActivity !== 'string' || !parseInt(sampleActivity)) {
+    return false
+  }
+let period = Math.ceil(
+  Math.log(MODERN_ACTIVITY / +sampleActivity) / 0.693 * HALF_LIFE_PERIOD)
+
+return period > 0 ? period : false
 }
 
 module.exports = {
